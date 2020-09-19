a1ebadf86db1ddc8d0faca2a43cba3388a597d80
EunKo <koeun0529@gmail.com>
2020-09-19 17:12:10 +0900

    set initial style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import styled from 'styled-components';
 
 function App() {
-	return <div></div>;
+	return <Wrapper>리액트 메신저</Wrapper>;
 }
 
 export default App;
+
+const Wrapper = styled.div`
+	height: 100vh;
+	& * {
+		font-family: sans-serif;
+	}
+`;
