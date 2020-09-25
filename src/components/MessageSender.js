4a609e3868c38f869d7a42a4253dc5bbcbf91fdb
yuna <ovo6v6@ewhain.net>
2020-09-25 11:25:57 +0900

    messageSender - 정적인 버전

--- src/components/MessageSender.js
+++ src/components/MessageSender.js
@@ -2,6 +2,34 @@ import React from 'react';
 import styled from 'styled-components';
 
 export default function MessageSender() {
-	return <Wrapper>메시지 전송부</Wrapper>;
+	return (
+		// 메시지 전송부
+		<Wrapper>
+			<input></input>
+			<button type="submit">전송</button>
+		</Wrapper>
+	);
 }
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+	display: flex;
+	flex-direction: row;
+	background-color: blue;
+	padding: 10px;
+
+	input {
+		margin-right: 10px;
+		flex: 1;
+		font-size: 15px;
+		height: 50px;
+		border: 1px solid rgba(0, 0, 0, 0.3);
+		border-radius: 15px;
+	}
+	button {
+		font-size:15px;
+		width: 65px;
+		height: 50px;
+		border: none;
+		border-radius: 20px;
+		background-color: rgb(255,224,102);
+	}
+`;
