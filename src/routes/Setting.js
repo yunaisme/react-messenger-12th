dc8891362625dace9e6748253f25349a2e644be0
yuna <ovo6v6@ewhain.net>
2020-11-04 15:36:34 +0900

    Setting 완성

--- src/routes/Setting.js
+++ src/routes/Setting.js
@@ -1,8 +1,57 @@
 import React from 'react';
-import './Setting.css';
+import styled from 'styled-components';
+import DEFAULT_PROFILE from '../img/default_profile.PNG';
 
 export default function Setting() {
     return(
-        <div>Setting</div>
+        <Wrapper>
+            <Title>설정</Title>
+            <Profile>
+                <ProfileImg src={DEFAULT_PROFILE} alt="default profile"></ProfileImg>
+                <ProfileDesc>
+                    <div>황유나</div>
+                    <div>kkkkkk@kkkk.com</div>
+                </ProfileDesc>
+            </Profile>
+            <Menu>
+                <MenuTab>새소식</MenuTab>
+                <MenuTab>정보</MenuTab>
+                <MenuTab>공지사항</MenuTab>
+                <MenuTab>도움말</MenuTab>
+                <MenuTab>문의하기</MenuTab>
+            </Menu>
+        </Wrapper>
     );
 }
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+const Title = styled.h1`
+    padding: 30px;
+    margin: 0;
+`;
+const Profile = styled.div`
+    display: flex;
+    margin: 20px 0 20px 40px;
+`;
+const ProfileImg = styled.img`
+    margin: 0;
+    border-radius: 20px;
+`;
+const ProfileDesc = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-left: 20px;
+`
+const Menu = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin: 20px 0 20px 40px;
+`;
+const MenuTab = styled.h3`
+    font-weight: normal;
+    padding-top: 20px;
+    margin: 0;
+`;
